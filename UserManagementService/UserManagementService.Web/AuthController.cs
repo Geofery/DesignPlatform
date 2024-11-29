30d8fcee4e7b6fad59b17c6a28460ce56208b90d
Roed <Geofery10@hotmail.com>
2024-11-29 10:21:28 +0100

    added application layer

--- UserManagementService/UserManagementService.Web/AuthController.cs
+++ UserManagementService/UserManagementService.Web/AuthController.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using UserManagementService.Domain;
 using UserManagementService.Infrastructure;
 
 
--- UserManagementService/UserManagementService.Web/Program.cs
+++ UserManagementService/UserManagementService.Web/Program.cs
@@ -6,9 +6,16 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<ITokenService, TokenService>();
 builder.Services.AddScoped<IUserService, UserService>();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "User Management API",
+        Version = "v1"
+    });
+}); builder.Services.AddScoped<ITokenService, TokenService>();
 
 
 var app = builder.Build();
